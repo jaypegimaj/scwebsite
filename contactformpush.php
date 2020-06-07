db541b04a4bfe69e9a90c102414e49a7c6973d91
jaypegimaj <48104953+jaypegimaj@users.noreply.github.com>
2020-06-07 15:48:06 -0700

    Update contactformpush.php

--- contactformpush.php
+++ contactformpush.php
@@ -1,59 +1,19 @@
 <?php
 
-$errors = ”;
-
-$myemail = ‘jayjclouse@gmail,com’;//<—–Put Your email address here. if(empty($_POST[‘name’]) ||
-
-empty($_POST[’email’]) ||
-
-empty($_POST[‘message’]))
-
-{
-
-$errors .= “\n Error: all fields are required”;
-
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
 }
 
-$name = $_POST[‘name’];
 
-$email_address = $_POST[’email’];
+$mailTo = "jayjclouse@yahoo.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an email from ".$name."./n/n".$message;
 
-$message = $_POST[‘message’];
 
-if (!preg_match(
-
-“/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”, $email_address))
-
-{
-
-$errors .= “\n Error: Invalid email address”;
-
-}
-
-if( empty($errors))
-
-{
-
-$to = $myemail;
-
-$email_subject = “Contact form submission: $name”;
-
-$email_body = “You have received a new message. “.
-
-” Here are the details:\n Name: $name \n “.
-
-“Email: $email_address\n Message \n $message”;
-
-$headers = “From: $myemail\n”;
-
-$headers .= “Reply-To: $email_address”;
-
-mail($to,$email_subject,$email_body,$headers);
-
-//redirect to the ‘thank you’ page
-
-header(‘Location: contact-form-thank-you.html’);
-
-}
+mail ($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
 
 ?>
